80e83ccf06d21e61130faa82613ebc45fad42786
general0303 <psh030302@gmail.com>
2022-05-02 15:29:21 +0300

    развертывание

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'it-messenger'
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE') or 'mysql+pymysql://petr:byudUJgEG6pkmS20@195.19.44.146:3306/petr'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:pass@localhost:3306/messenger'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE') or 'mysql+pymysql://petr:byudUJgEG6pkmS20@195.19.44.146:3306/petr'
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:pass@localhost:3306/messenger'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'it-messenger'
     UPLOAD_FOLDER = './static/files'
